a9abf36ad8b0e3255f861be82141991ea5d738f5
Vladas Zakrevskis <146100@gmail.com>
2019-07-10 14:19:23 +0300

    st, push

---
+++ push.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+all = sys.argv
+
+if len(sys.argv) < 2:
+    os.system("git push")
+else:
+    os.system("git add -A")
+    os.system("git commit -m " + sys.argv[1])
+    os.system("git push")
