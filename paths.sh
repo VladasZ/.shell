daa391e018607b721ecea3a2b34616f7b090a84d
Vladas <146100@gmail.com>
2020-01-21 22:47:58 +0300

    pu pi

--- paths.sh
+++ paths.sh
@@ -7,6 +7,14 @@ function tavi {
   cd ~/dev/work/atom/Taavi-iOS
 }
 
+function pi {
+	pod install
+}
+
+function pu {
+	pod update
+}
+
 export PATH=$PATH:~/.shell
 export PATH=$PATH:~/.shell/git
 export PATH=$PATH:~/.shell/ios
