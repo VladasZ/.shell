bd04ebd66e270555353c9f8196b302f99db730ba
Vladas Zakrevskis <146100@gmail.com>
2020-08-31 16:20:22 +0300

    clap util

---
+++ tools/clap_dll.py
@@ -0,0 +1,13 @@
+import File
+
+unity_path = "C:/Users/vladas/dev/work/tes/glove/GloveSoftware/Unity/Assets/CLAP/Core/Dlls/"
+
+clap_dll = unity_path + "CLAPTracker.dll"
+custom_dll = unity_path + "CLAPTrackerCustom.dll"
+built_dll = "C:/Users/vladas/dev/work/tes/glove/GloveSoftware/glove_plugin/build/bin/clap_glove_dll.dll"
+
+
+File.rm(clap_dll)
+File.rm(custom_dll)
+File.copy(built_dll, clap_dll)
+File.copy(built_dll, custom_dll)
