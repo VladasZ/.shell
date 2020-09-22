3ccc900b6e9e57eca9e1b9c7a84425ace1c81473
Vladas Zakrevskis <146100@gmail.com>
2020-09-22 18:41:12 +0300

    clap

--- tools/clap_dll.py
+++ tools/clap_dll.py
@@ -3,19 +3,16 @@ import File
 
 home = expanduser("~/")
 
-#C:\Users\pc\dev\work\tes\glove\GloveSoftware\ClapXRGlove\Assets\CLAP\Core\Bin\x64
-
 glove_path = home + "dev/work/tes/glove/GloveSoftware/"
 
-unity_path = glove_path + "Unity/Assets/CLAP/Core/Dlls/"
-new_unity_path = glove_path + "ClapXRGlove/Assets/CLAP/Core/Bin/x64/"
+unity_path = glove_path + "GloveUnity/Assets/CLAP/Core/Bin/x64/"
 
 
 def remove_and_copy_at(path):
 
     global home
 
-    built_dll = home + "dev/work/tes/glove/GloveSoftware/glove_plugin/build/bin/clap_glove_dll.dll"
+    built_dll = home + "dev/work/tes/teslasuit-studio/bin/Debug/clap_glove_dll.dll"
 
     clap_dll   = "CLAPTracker.dll"
     custom_dll = "CLAPTrackerCustom.dll"
@@ -28,4 +25,3 @@ def remove_and_copy_at(path):
 
 
 remove_and_copy_at(unity_path)
-remove_and_copy_at(new_unity_path)
